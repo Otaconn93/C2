285991a4a7cb23fe7e8fd4ffd60434489fa165f5
Shamil Gradov <shamil.gradov@hochschule-stralsund.de>
2019-11-06 19:21:41 +0100

    Familienklasse fertiggestellt

--- src/Familie.java
+++ src/Familie.java
@@ -4,19 +4,39 @@ import java.util.ArrayList;
  * @author Shamil Gradov
  */
 public class Familie {
-    private ArrayList mitglieder;
+    private ArrayList<String> mitglieder = new ArrayList<String>();
 
     public Familie(String vater, String mutter) throws IllegalArgumentException{
         if(vater.trim().equals("")||mutter.trim().equals(""))
             throw new IllegalArgumentException("Name darf nicht leer sein");
+
+        mitglieder.add(vater);
+        mitglieder.add(mutter);
     }
     public void addKind(String kind)throws IllegalArgumentException{
 
         if (kind.trim().equals(""))
             throw new IllegalArgumentException("Name darf nicht leer sein");
+
+        mitglieder.add(kind);
     }
     public enum Familienmitglied{
-// Ihr Code hier
+        VATER,MUTTER,KINDER
     }
-// implementieren Sie hier die Methode "getMitglied(...)
+    public String getMitglied(Familienmitglied person){
+        if(person==Familienmitglied.VATER){
+            return mitglieder.get(0);
+        }
+        if(person==Familienmitglied.MUTTER){
+            return mitglieder.get(1);
+        }
+        if(person==Familienmitglied.KINDER){
+            ArrayList<String> kinder = new ArrayList<String>();
+            for(int i=2;i<mitglieder.size();i++){
+                kinder.add(mitglieder.get(i));
+            }
+            return String.join(",",kinder);
+        }
+        return null;
+    }
 }
